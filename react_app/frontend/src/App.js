c99787a2bb658d7f59181af54f8a3031757594f7
kuroishihiromu <kuroishihiromu@gmail.com>
2023-12-13 01:06:26 +0900

    Login,SignUp:react側

--- react_app/frontend/src/App.js
+++ react_app/frontend/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Login from './components/Login';
+import SignUp from './components/Signup'
 
 function App() {
   return (
     <div className="text-4xl text-center">
-      <header className="">
-        unchicchi
+      <header className="my-20">
+        App Title
       </header>
+      <div>
+        <img src='/img/tree-icon.png' alt='tree-icon' className='w-[60%] mx-auto my-20'></img>
+      </div>
+      <Login />
+      <SignUp />
     </div>
   );
 }
---
+++ react_app/frontend/src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showForm, setShowForm] = useState(false);
+
+    const handleLoginToggle = () => {
+        // ボタンをクリックするたびに表示と非表示をトグル
+        setShowForm((prevShowForm) => !prevShowForm);
+    };
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('/api/login', {
+                username: username,
+                password: password,
+            });
+            console.log('ログイン成功:', response.data);
+
+            // ログイン成功時の追加の処理をここに追加
+
+            window.location.href = '/home';
+
+        } catch (error) {
+            console.error('ログインエラー', error);
+        }
+    };
+
+    return (
+        <div className="w-[60%] mx-auto bg-red rounded-[20px] mt-10 px-4 py-0 bg-blue-500">
+            <button onClick={handleLoginToggle} className="text-[16px]  text-white px-2 py-1 rounded-md">ログイン</button>
+            {showForm && (
+                <div className="text-[14px] mt-2">
+                    <div>
+                        <label htmlFor="username">ユーザー名:</label>
+                        <input 
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className="border rounded-md px-2 py-1"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="mt-2">パスワード:</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="border rounded-md px-2 py-1"
+                        />
+                    </div>
+                    <button onClick={handleLogin} className="bg-green-500 text-white px-2 rounded-md my-2">送信</button>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Login;
